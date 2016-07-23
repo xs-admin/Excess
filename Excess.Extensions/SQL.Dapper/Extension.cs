005790c0e37a5499f06fa882611e71c4de2adae9
xs-admin <emilio.santos@outlook.com>
2016-07-22 23:57:50 -0500

    dapper 0.4

---
+++ Excess.Extensions/SQL.Dapper/Extension.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Excess.Compiler;
+using Excess.Compiler.Roslyn;
+using Excess.Compiler.Attributes;
+
+namespace SQL.Dapper
+{
+    using ExcessCompiler = Excess.Compiler.ICompiler<SyntaxToken, SyntaxNode, SemanticModel>;
+    using CSharp = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+    [Extension("sql")]
+    public class DapperExtension
+    {
+        public static void Apply(ExcessCompiler compiler, Scope scope = null)
+        {
+            scope?.AddKeywords("sql");
+            compiler.Lexical().extension("sql", ExtensionKind.Code, ParseDapper);
+        }
+
+        private static SyntaxNode ParseDapper(SyntaxNode node, Scope scope, LexicalExtension<SyntaxToken> extension)
+        {
+            var tokenCount = extension.Body.Count();
+            var withoutBraces = extension
+                .Body
+                .Skip(1)
+                .Take(tokenCount - 2);
+
+            if (node is LocalDeclarationStatementSyntax)
+                return ParseQuery(node as LocalDeclarationStatementSyntax, scope, withoutBraces);
+            else if (node is ExpressionStatementSyntax)
+                return ParseCommand(node as ExpressionStatementSyntax, scope, withoutBraces);
+
+            Debug.Assert(false); //td: error
+            return node;
+        }
+
+        private static Template DapperQuery = Template.ParseExpression("__connection.Query<__0>(__1, __2)");
+        private static SyntaxNode ParseQuery(LocalDeclarationStatementSyntax node, Scope scope, IEnumerable<SyntaxToken> body)
+        {
+            var declaration = node.Declaration;
+            var variable = declaration.Variables.Single(); //td: error
+            var type = declaration.Type;
+            if (!ValidateQueryType(type, out type))
+            {
+                Debug.Assert(false); //td: error
+                return node;
+            }
+
+            var parameters = default(AnonymousObjectCreationExpressionSyntax);
+            var sqlQueryString = ParseBody(body, scope, out parameters);
+            if (sqlQueryString == null)
+            {
+                Debug.Assert(false); //td: error
+                return node;
+            }
+
+            return node
+                .WithDeclaration(node.Declaration
+                    .WithVariables(CSharp.SeparatedList(new[] {
+                        variable.WithInitializer(variable.Initializer
+                            .WithValue(DapperQuery.Get<ExpressionSyntax>(
+                                type,
+                                sqlQueryString,
+                                parameters)))})));
+        }
+
+        private static Template DapperCommand = Template.ParseExpression("__connection.Execute(__0, __1)");
+        private static SyntaxNode ParseCommand(ExpressionStatementSyntax node, Scope scope, IEnumerable<SyntaxToken> body)
+        {
+            var parameters = default(AnonymousObjectCreationExpressionSyntax);
+            var sqlQueryString = ParseBody(body, scope, out parameters);
+            if (sqlQueryString == null)
+            {
+                Debug.Assert(false); //td: error
+                return node;
+            }
+
+            return node.WithExpression( 
+                DapperCommand.Get<ExpressionSyntax>(
+                    sqlQueryString, parameters));
+        }
+
+        private static bool ValidateQueryType(TypeSyntax type, out TypeSyntax innerType)
+        {
+            innerType = null;
+            if (type is GenericNameSyntax)
+            {
+                var generic = type as GenericNameSyntax;
+                if (generic.Arity != 1)
+                {
+                    Debug.Assert(false); //td: error
+                    return false;
+                }
+
+                innerType = generic
+                    .TypeArgumentList
+                    .Arguments
+                    .Single();
+
+                return true;
+            }
+
+            Debug.Assert(false); //td: error
+            return false;
+        }
+
+        private static ExpressionSyntax ParseBody(IEnumerable<SyntaxToken> body, Scope scope, out AnonymousObjectCreationExpressionSyntax parameters)
+        {
+            parameters = null;
+
+            var builder = new StringBuilder();
+            var @params = new List<AnonymousObjectMemberDeclaratorSyntax>();
+            builder.Append("@\"");
+            foreach (var token in body)
+            {
+                var str = token.ToFullString();
+                if (token.IsKind(SyntaxKind.IdentifierToken) && str.StartsWith("@"))
+                {
+                    var newToken = CSharp.ParseToken(str.Substring(1))
+                        .WithAdditionalAnnotations(token.GetAnnotations());
+
+                    @params.Add(CSharp.AnonymousObjectMemberDeclarator(
+                        CSharp.NameEquals(CSharp.IdentifierName(newToken)),
+                        CSharp.IdentifierName(newToken)));
+                }
+
+                builder.Append(str);
+            }
+            builder.Append("\"");
+
+            var result = CSharp.ParseExpression(builder.ToString());
+            if (result == null)
+            {
+                Debug.Assert(false); //td: error
+                return null;
+            }
+
+            parameters = CSharp.AnonymousObjectCreationExpression(CSharp.SeparatedList(
+                @params));
+
+            return result;
+        }
+    }
+}
---
+++ Excess.Extensions/Tests/DapperTests.cs
@@ -0,0 +1,134 @@
+﻿using Excess.Compiler.Mock;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQL.Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class DapperTests
+    {
+        [TestMethod]
+        public void Query_Usage()
+        {
+            var tree = ExcessMock.Compile(@"
+                class SomeClass
+                {
+                    void SomeMethod(int SomeInt)
+                    {
+                        IEnumerable<SomeModel> result = sql
+                        {
+                            select * from SomeTable
+                            where SomeColumn > @SomeInt
+                        }
+                    }
+                }", builder: (compiler) => DapperExtension.Apply(compiler));
+
+            //must have a call to __connection.Query
+            var invocation = (tree.GetRoot()
+                .DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Single()
+                .Body
+                    .Statements
+                    .Single() as LocalDeclarationStatementSyntax)
+                        .Declaration
+                        .Variables
+                        .Single()
+                        .Initializer
+                            .Value as InvocationExpressionSyntax;
+
+            Assert.IsNotNull(invocation);
+            Assert.IsTrue(invocation is InvocationExpressionSyntax);
+            Assert.IsTrue((invocation as InvocationExpressionSyntax)
+                .Expression
+                .ToString()
+                .StartsWith("__connection.Query"));
+
+            //must have added a literal string @"...query..."
+            var literal = tree.GetRoot()
+                .DescendantNodes()
+                .OfType<LiteralExpressionSyntax>()
+                .Single()
+                .ToString();
+
+            Assert.IsTrue(literal.Contains("@\""));
+            Assert.IsTrue(literal.Contains("where SomeColumn > @SomeInt"));
+
+            //must have added parameters as an anonymous object
+            var parameters = tree.GetRoot()
+                .DescendantNodes()
+                .OfType<AnonymousObjectCreationExpressionSyntax>()
+                .Single();
+
+            Assert.IsTrue(parameters
+                .Initializers
+                .Any(init => init
+                    .NameEquals
+                    .Name
+                    .ToString() == "SomeInt"));
+        }
+
+        [TestMethod]
+        public void Command_Usage()
+        {
+            var tree = ExcessMock.Compile(@"
+                class SomeClass
+                {
+                    void SomeMethod(int SomeInt)
+                    {
+                        result = sql
+                        {
+                            insert into SomeTable
+                            values(@SomeInt)
+                        }
+                    }
+                }", builder: (compiler) => DapperExtension.Apply(compiler));
+
+            //must have a call to __connection.Execute
+            var invocation = (tree.GetRoot()
+                .DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Single()
+                .Body
+                    .Statements
+                    .Single() as ExpressionStatementSyntax)
+                        .Expression as InvocationExpressionSyntax;
+
+            Assert.IsNotNull(invocation);
+            Assert.IsTrue(invocation is InvocationExpressionSyntax);
+            Assert.IsTrue((invocation as InvocationExpressionSyntax)
+                .Expression
+                .ToString()
+                .StartsWith("__connection.Execute"));
+
+            //must have added a literal string @"...query..."
+            var literal = tree.GetRoot()
+                .DescendantNodes()
+                .OfType<LiteralExpressionSyntax>()
+                .Single()
+                .ToString();
+
+            Assert.IsTrue(literal.Contains("@\""));
+            Assert.IsTrue(literal.Contains("values(@SomeInt)"));
+
+            //must have added parameters as an anonymous object
+            var parameters = tree.GetRoot()
+                .DescendantNodes()
+                .OfType<AnonymousObjectCreationExpressionSyntax>()
+                .Single();
+
+            Assert.IsTrue(parameters
+                .Initializers
+                .Any(init => init
+                    .NameEquals
+                    .Name
+                    .ToString() == "SomeInt"));
+        }
+    }
+}
